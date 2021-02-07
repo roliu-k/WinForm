6708c0c4d2e6aed72dfe715a7da025588ffc6b33
ricobadico <biondiceric@gmail.com>
2021-02-06 19:21:38 -0700

    started template for product gridview

---
+++ CMPP248_Workshop/TravelExpertDatabase/PackageRelatedTableData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelExpertDatabase
+{
+    /*
+     * 
+     * Provides the structure for the data used in the 
+     * datagrid for product info related to a package.
+     * Note this class is used for cosmetic purposes only,
+     * as an initial datasource so that the datagrid can
+     * be easily formatted in design view.
+     * The actual getting of this data is done directly
+     * through a LINQ query (TravelExpertsQueryManager.FindProdInfoByPackage)
+     * 
+     */
+
+    class PackageRelatedTableData
+    {
+        public int PackageId { get; set; }
+        public int ProductId { get; set; }
+        public string ProdName { get; set; }
+        public int SupplierId { get; set; }
+        public string Supplier { get; set; }
+    }
+}
