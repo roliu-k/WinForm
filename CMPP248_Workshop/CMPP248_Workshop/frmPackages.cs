afe68e9b6a4795c18b30aecf79257c6764dddd9a
ricobadico <biondiceric@gmail.com>
2021-02-06 17:32:11 -0700

    modify button now takes the id of the grid's selected row

--- CMPP248_Workshop/CMPP248_Workshop/frmPackages.cs
+++ CMPP248_Workshop/CMPP248_Workshop/frmPackages.cs
@@ -11,7 +11,18 @@ using TravelExpertDatabase;
 
 namespace CMPP248_Workshop
 {
-    // Displays Packages.
+    /*
+     * Displays packages in read-only context,
+     * with buttons allowing the user to proceed
+     * to add new packages or modify the selected.
+     * 
+     * The initial bulk of this form was done as
+     * a group in a live session, creating a functioning
+     * details-view layout that allowed the user to 
+     * direct to the proper add or modify form. 
+     * Additional individual fixes are noted, but all work
+     * otherwise was distributed equally.
+     */
 
     public partial class frmPackages : Form
     {
@@ -27,7 +38,7 @@ namespace CMPP248_Workshop
         {
             packageBindingSource.DataSource = new travelexpertsDataContext().Packages;
 
-            // add data to Packages grid
+            // Add data to Packages gridview [Eric - changed from details view to gridview, to allow easier browsing]
             grdPackages.DataSource = new travelexpertsDataContext().Packages;
         }
 
@@ -49,22 +60,25 @@ namespace CMPP248_Workshop
         //Allows modifications to Packages
         private void btnModify_Click(object sender, EventArgs e)
         {
+            // Grab ID of the row currently selected in the Packages Datagrid. [Eric]
+            int selectedId = Convert.ToInt32(grdPackages.CurrentRow.Cells[0].Value);
+
             //Package currentPackage;
             using (travelexpertsDataContext dbContext = new travelexpertsDataContext())
             {
                 currentPackage = (from p in dbContext.Packages // LINQ query that returns one record
-                                  where p.PackageId.ToString() == packageIdTextBox.Text
+                                  where p.PackageId == selectedId // use ID of selected row in Packages
                                   select p).Single(); // method Single runs the LINQ query, only when receiving one value
 
 
                 frmAddModify secondForm = new frmAddModify();
-                    secondForm.isAdd = false; // Modify mode
-                    secondForm.currentPackage = currentPackage;
-                    DialogResult result = secondForm.ShowDialog(); // display second form modal
-                    if (result == DialogResult.OK || result == DialogResult.Retry) // successful update or concurrency exception
-                    {
-                        RefreshView();
-                    }
+                secondForm.isAdd = false; // Modify mode
+                secondForm.currentPackage = currentPackage;
+                DialogResult result = secondForm.ShowDialog(); // display second form modal
+                if (result == DialogResult.OK || result == DialogResult.Retry) // successful update or concurrency exception
+                {
+                    RefreshView();
+                }
             }
         }
 
